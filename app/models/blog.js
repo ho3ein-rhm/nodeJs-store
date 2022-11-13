ed97ef5243cb3f5da38a2da16eadfca6ca5dae71
ho3ein-rhm <63186954+ho3ein-rhm@users.noreply.github.com>
2022-11-13 18:28:23 +0330

    finile Edit to Schema 1.3

--- app/models/blog.js
+++ app/models/blog.js
@@ -1,7 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const schema = new mongoose.Schema({})
+const schema = new mongoose.Schema({
+  author: { type: mongoose.Types.ObjectId, required: true },
+  title: { type: String },
+  text: { type: String },
+  images: { type: String },
+  tags: { type: [String], default: [] },
+  category: { type: mongoose.Types.ObjectId, required: true },
+  cumments: { type: [], default: [] },
+  like: { type: [mongoose.Types.ObjectId], default: [] },
+  deslike: { type: [mongoose.Types.ObjectId], default: [] },
+  bookmark: { type: [mongoose.Types.ObjectId], default: [] },
+});
 
 module.exports = {
-    blogSchema: mongoose.model('', schema)
-}
+  blogSchema: mongoose.model("", schema),
+};
