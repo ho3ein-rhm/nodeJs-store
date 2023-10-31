7b5870b7358f374e8c3b52d862372255069b7ea7
ho3ein-rhm <63186954+ho3ein-rhm@users.noreply.github.com>
2023-10-31 14:11:29 +0330

    category

--- app/http/controllers/admin/category.controller.js
+++ app/http/controllers/admin/category.controller.js
@@ -8,7 +8,6 @@ class categoryController extends Controller {
       console.log(req.body);
       await categorySchema.validateAsync(req.body);
       const { title, parent } = req.body;
-      console.log(typeof parent);
       const category = await this.models.categoryModel.create({
         title,
         parent,
@@ -19,7 +18,91 @@ class categoryController extends Controller {
         message: "creation succesfully",
       });
     } catch (error) {
+      next(error);
+    }
+  }
+  async readparentCategory(req, res, next) {
+    try {
+      this.models.categoryModel.find({ parent: undefined }, (err, category) => {
+        if (err)
+          throw createHttpError.InternalServerError("داده ای  پیدا نشد!");
+        return res.json({
+          data: {
+            category,
+          },
+          statusCode: 200,
+        });
+      });
+    } catch (error) {
       console.log(error);
+      next(error);
+    }
+  }
+  async getChildrenCategory(req, res, next) {
+    try {
+      const { parent } = req.params;
+      const children = await this.models.categoryModel.find({ parent });
+      if (Object.keys(children).length == 0)
+        throw createHttpError.NotFound("دسته بندی پیدا نشد");
+      res.status(200).json({
+        data: {
+          type: typeof children,
+          children,
+        },
+        statusCode: 200,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async deleteCategory(req, res, next) {
+    try {
+      const { id } = req.params;
+      const category = await this.checkExistCategory(id);
+      const deletedcategory = await this.models.categoryModel.deleteOne({
+        _id: category._id,
+      });
+      if (deletedcategory.deletedCount == 0)
+        throw createHttpError.InternalServerError("خطایی رخ داده است ");
+      res.status(200).json({
+        message: "دسته بندی با  موفقیت حذف شد",
+        statusCode: 200,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async checkExistCategory(id) {
+    const category = await this.models.categoryModel.findById(id);
+    if (!category) throw createHttpError.NotFound("داده ای  پیدا نشد ");
+    return category;
+  }
+  async getAllCategorys(req, res, next) {
+    try {
+      const category = await this.models.categoryModel.aggregate([
+        {
+          $lookup: {
+            from: "categories",
+            localField: "_id",
+            foreignField: "parent",
+            as: "children",
+          },
+        },
+        {
+          $project: {
+            __v: 0,
+            "children.parent": 0,
+            "children.__v": 0,
+          },
+        },
+      ]);
+      res.status(200).json({
+        data: {
+          category,
+        },
+        statusCode: 200,
+      });
+    } catch (error) {
       next(error);
     }
   }
--- app/router/admin/category.routes.js
+++ app/router/admin/category.routes.js
@@ -5,6 +5,10 @@ const {
 const router = require("express").Router();
 
 router.post("/add", categoryController.addcategory);
+router.get("/parent", categoryController.readparentCategory);
+router.get("/children/:parent", categoryController.getChildrenCategory);
+router.delete("/delete-category/:id", categoryController.deleteCategory);
+router.get("/all", categoryController.getAllCategorys);
 
 module.exports = {
   categoryRoutes: router,
